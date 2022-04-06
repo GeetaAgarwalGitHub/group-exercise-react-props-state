d461a2bb7c22a47ba790caa7b5e0027abd917eaa
Vijay Agarwal <AGARWAV@domesticandgeneral.com>
2022-04-06 14:38:59 +0100

    Update chuck_card.tsx
    Task 1 : Replaced text with image on the card

--- src/components/chuck_card.tsx
+++ src/components/chuck_card.tsx
@@ -1,12 +1,11 @@
-import ChuckImage from './assets/chuck_norris.jpg';
+import ChuckImage from '../assets/chuck_norris.jpg';
 
 const ChuckCard :React.FC<{greeting:string}>= (props)  =>
 
         <>
             <h2>{props.greeting}</h2>
 
-            <img src={ 'Chuck image here!'} alt='A handsome man' /> 
+            <img src={ChuckImage} alt='A handsome man' /> 
         </>
-
 
 export default ChuckCard;
