4ae5acad77b563c1aa3d9f096c85e4aadf915540
Vijay Agarwal <AGARWAV@domesticandgeneral.com>
2022-04-06 15:19:00 +0100

    Update chuck_info.tsx
    Task 2  : displayed counters

--- src/components/chuck_info.tsx
+++ src/components/chuck_info.tsx
@@ -1,8 +1,8 @@
-const ChuckInfo = () => 
+const ChuckInfo:React.FC<{whalesSaved:number,roundHouseKicks: number }>= (props) => 
         <>
-            <p>Number of Whales Saved: </p>
+            <p>Number of Whales Saved: {props.whalesSaved} </p>
 
-            <p>Number of Round House Kicks (in the last day): </p>
+            <p>Number of Round House Kicks (in the last day):{props.roundHouseKicks} </p>
         </>
     
 
