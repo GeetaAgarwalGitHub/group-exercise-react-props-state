9a942a576ca56b6c8227503444e8cc10d75d3ef2
Vijay Agarwal <AGARWAV@domesticandgeneral.com>
2022-04-06 15:13:32 +0100

    Update chuck_jokes.tsx
    Task 3 : Display jokes

--- src/components/chuck_jokes.tsx
+++ src/components/chuck_jokes.tsx
@@ -1,10 +1,13 @@
-// An alternative way of declaring a component is to write it as a function which
+
+
+import Joke from "../joke";
+
 // returns a JSX.Element 
-function ChuckJokes() : JSX.Element {
+function ChuckJokes(joke: string) : JSX.Element {
 
     return (
         <>
-            <p></p>
+            <p>{joke}</p>
         </>
     )
 }
